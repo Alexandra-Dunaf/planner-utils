2f957d19d92a8c982d5db108a3a9f23f1b07f3f3
Alexandra_Dunaf <sasha-kalugina@yandex.ru>
2023-09-29 21:18:19 +0400

    transfer User of their monolithic application

---
+++ src/main/java/ru/dunaf/planner/utils/aop/LoggingAspect.java
@@ -0,0 +1,43 @@
+package ru.dunaf.planner.utils.aop;
+
+import lombok.extern.java.Log;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+@Log
+public class LoggingAspect {
+
+    //аспект будет выполняться для всех методов из пакета контроллеров
+    @Around("execution(* ru.dunaf.planner.todo.controller..*(..)))")
+    public Object profileControllerMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
+    {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        // получить информацию о том, какой класс и метод выполняется
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        log.info("-------- Executing "+ className + "." + methodName + "   ----------- ");
+
+        StopWatch stopWatch = new StopWatch();
+
+        //засекаем время
+        stopWatch.start();
+
+        // выполняем сам метод
+        Object result = proceedingJoinPoint.proceed();
+
+        stopWatch.stop();
+
+        log.info("-------- Executing "+ className + "." + methodName + " : " + stopWatch.getTotalTimeMillis() + "   ----------- ");
+
+        return result;
+    }
+
+}
